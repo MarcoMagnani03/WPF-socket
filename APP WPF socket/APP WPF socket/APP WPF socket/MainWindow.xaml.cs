eb6f07bb86f8b8fcdae4287312f852b21f24f53e
Marco <marco.magnani.stud@ispascalcomandini.it>
2021-05-03 10:54:07 +0200

    Aggiunto controllo errori, immagini e prima parte morra cinese

--- APP WPF socket/APP WPF socket/APP WPF socket/MainWindow.xaml.cs
+++ APP WPF socket/APP WPF socket/APP WPF socket/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace APP_WPF_socket
     /// </summary>
     public partial class MainWindow : Window
     {
+        static private string simbolo;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,10 +36,11 @@ namespace APP_WPF_socket
             IPEndPoint sourceSocket = new IPEndPoint(IPAddress.Parse("192.168.1.194"),56000);
             //creiamo il destination socket. Insserendo i valori inseriti nelle rispettive textBox
             string[] numeroIp = txtIP.Text.Split('.');
-            btnInvia.IsEnabled = true;
+            btnGioca.IsEnabled = true;
+            //controllo contenuto delle textBox
             if (numeroIp.Length != 4)
             {
-                btnInvia.IsEnabled = false;
+                btnGioca.IsEnabled = false;
                 lblErrore.Content = "L'indirizzo IP selezionato è scorretto";
                 lblErrore.Visibility = Visibility.Visible;
             }
@@ -47,18 +49,18 @@ namespace APP_WPF_socket
             {
                 if (!int.TryParse(numeroIp[i],out tmp))
                 {
-                    btnInvia.IsEnabled = false;
+                    btnGioca.IsEnabled = false;
                     lblErrore.Content = "L'indirizzo IP selezionato è scorretto, sono presenti dei caratteri scorretti";
                     lblErrore.Visibility = Visibility.Visible;
                 }
             }
-            if (btnInvia.IsEnabled)
+            if (btnGioca.IsEnabled)
             {
                 for (int i = 0; i < numeroIp.Length; i++)
                 {
                     if (int.Parse(numeroIp[i]) < 0 || int.Parse(numeroIp[i]) > 255)
                     {
-                        btnInvia.IsEnabled = false;
+                        btnGioca.IsEnabled = false;
                         lblErrore.Content = $"L'indirizzo IP selezionato contiene un valore inferiore allo 0 o superiore al 255";
                         lblErrore.Visibility = Visibility.Visible;
                     }
@@ -68,7 +70,7 @@ namespace APP_WPF_socket
             {
                 lblErrore.Content = "La porta inserita è scorretta";
             }
-            if (btnInvia.IsEnabled)
+            if (btnGioca.IsEnabled)
             {
                 lblErrore.Visibility = Visibility.Visible;
             }
@@ -99,26 +101,64 @@ namespace APP_WPF_socket
                     if (t.Available > 0)
                     {
                         message = "";
+                        string tmp="";
                         //quello che riceve sul socket lo codifica in ASCII e lo mette dentro message
                         contaCaratteri = t.Receive(bytesRicevuti,bytesRicevuti.Length,0);
                         message = message + Encoding.ASCII.GetString(bytesRicevuti,0,contaCaratteri);
                         //si usa quando nei thread bisogna aggiornare le interfacce. Se non lo si usa da errore
+                        switch (message)
+                        {
+                            //QUI controllo quale dei due utenti ha vinto controllando le varie casistiche
+                            case "sasso":
+                                switch (simbolo)
+                                {
+                                    case "sasso":
+                                       tmp = "Anche il tuo avversaro ha scelto sasso è un PAREGGIO";
+                                        break;
+                                    case "carta":
+                                        tmp = "Il tuo avversario ha scelto sasso hai VINTO";
+                                        break;
+                                    case "forbici":
+                                        tmp = "Il tuo avversario ha scelto sasso hai PERSO";
+                                        break;
+                                }
+                                break;
+                            case "carta":
+                                switch (simbolo)
+                                {
+                                    case "sasso":
+                                        tmp = "Il tuo avversario ha scelto carta hai PERSO";
+                                        break;
+                                    case "carta":
+                                        tmp = "Anche il tuo avversaro ha scelto carta è un PAREGGIO";
+                                        break;
+                                    case "forbici":
+                                        tmp = "Il tuo avversario ha scelto carta hai VINTO";
+                                        break;
+                                }
+                                break;
+                            case "forbici":
+                                switch (simbolo)
+                                {
+                                    case "sasso":
+                                        tmp = "Il tuo avversario ha scelto forbici hai VINTO";
+                                        break;
+                                    case "carta":
+                                        tmp = "Il tuo avversario ha scelto forbici hai PERSO";
+                                        break;
+                                    case "forbici":
+                                        tmp = "Anche il tuo avversaro ha scelto forbici è un PAREGGIO";
+                                        break;
+                                }
+                                break;
+                        }
                         this.Dispatcher.BeginInvoke(new Action(() =>
                         {
-                            lblRicevi.Content = message;
+                            lblVittoria.Content = tmp;
                         }));
                     }
                 }
             });
-        }
-        private void btnInvia_Click(object sender, RoutedEventArgs e)
-        {
-            //Aggiungere controlli sul contenuto delle textbox
-            string ipAddress = txtIP.Text;
-            int port = int.Parse(txtPort.Text);
-            string messaggioInviato = $"Messaggio inviato: {txtMessage.Text} al socket {ipAddress} : {port} il {DateTime.Now}";
-            lstInviati.Items.Add(messaggioInviato);
-            SocketSend(IPAddress.Parse(ipAddress), port, txtMessage.Text);
         }
         public void SocketSend(IPAddress dest,int destport,string message)
         {
@@ -129,6 +169,56 @@ namespace APP_WPF_socket
             //Andiamo a creare il socket del destinatario
             IPEndPoint remote_endpoint=new IPEndPoint(dest,destport);
             s.SendTo(byteInviati, remote_endpoint);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            simbolo = ((Button)sender).Name.Substring(3);
+            switch (simbolo)
+            {
+                case "sasso":
+                    btncarta.IsEnabled = true;
+                    btnsasso.IsEnabled = false;
+                    btnforbici.IsEnabled = true;
+                    break;
+                case "carta":
+                    btncarta.IsEnabled = false;
+                    btnsasso.IsEnabled = true;
+                    btnforbici.IsEnabled = true;
+                    break;
+                case "forbici":
+                    btncarta.IsEnabled = true;
+                    btnsasso.IsEnabled = true;
+                    btnforbici.IsEnabled = false;
+                    break;
+            }
+            btnConferma.IsEnabled = true;
+        }
+
+        private void btnGioca_Click(object sender, RoutedEventArgs e)
+        {
+            Height = 400;
+            txtIP.Visibility = Visibility.Hidden;
+            txtPort.Visibility = Visibility.Hidden;
+            lblIndirizzoIp.Visibility = Visibility.Hidden;
+            lblPorta.Visibility = Visibility.Hidden;
+            btncarta.Visibility = Visibility.Visible;
+            btnforbici.Visibility = Visibility.Visible;
+            btnsasso.Visibility = Visibility.Visible;
+            btnCreaSocket.Visibility = Visibility.Hidden;
+            lblFaiScelta.Visibility = Visibility.Visible;
+            btnGioca.Visibility = Visibility.Hidden;
+            imgCarta.Visibility = Visibility.Visible;
+            imgForbici.Visibility = Visibility.Visible;
+            imgSasso.Visibility = Visibility.Visible;
+            btnConferma.Visibility = Visibility.Visible;
+        }
+
+        private void btnConferma_Click(object sender, RoutedEventArgs e)
+        {
+            string ipAddress = txtIP.Text;
+            int port = int.Parse(txtPort.Text);
+            SocketSend(IPAddress.Parse(ipAddress), port, simbolo);
         }
     }
 }
